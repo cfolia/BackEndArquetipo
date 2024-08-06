882dca241981b090038562adebf5358381017d9d
cfolia <cfolia@emergya.com>
2024-08-06 11:53:01 +0200

    syso

--- src/main/java/com/spring/crud/demo/SpringBootH2CRUDApplication.java
+++ src/main/java/com/spring/crud/demo/SpringBootH2CRUDApplication.java
@@ -24,6 +24,7 @@ import java.util.List;
 public class SpringBootH2CRUDApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hola");
 		SpringApplication.run(SpringBootH2CRUDApplication.class, args);
 	}
 

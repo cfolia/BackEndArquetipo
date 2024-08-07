8582b02d7fd7be0e88f95d9720fc9e7ecf937eab
Ahirtle <ahirtle@ws-ahirtle.com>
2024-08-07 08:51:27 +0200

    Añadir datos de estudiantes

--- src/main/java/com/spring/crud/demo/controller/DefaultController.java
+++ src/main/java/com/spring/crud/demo/controller/DefaultController.java
@@ -1,12 +1,13 @@
 package com.spring.crud.demo.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
+import java.net.URI;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.URI;
+import io.swagger.v3.oas.annotations.Operation;
 
 @RestController
 public class DefaultController {
